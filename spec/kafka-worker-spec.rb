9731f87b47518b46824be18857a0ae38c35a9adb
James Haver II <mchaver@gmail.com>
2017-04-19 19:04:49 +0800

    Add tests to check that Handler receives a message from kafka and that it properly handles an error in the handle method

--- spec/kafka-worker-spec.rb
+++ spec/kafka-worker-spec.rb
@@ -2,36 +2,45 @@ require 'kafka'
 require 'kafka-worker'
 require 'rspec'
 
-=begin
-if Rails.env.development?
-  $kafka = Kafka.new(
-    seed_brokers: (ENV['KAFKA_ENDPOINTS'] || "localhost").split(","),
-    client_id: "ctbweb",
-    logger: Rails.logger
-  )
-else
-  $kafka = Kafka.new(
-    seed_brokers: (ENV['KAFKA_ENDPOINTS'] || "localhost").split(","),
-    client_id: "ctbweb",
-    logger: Rails.logger,
-    ssl_ca_cert: File.read('ssl/ca-cert.pem'),
-    ssl_client_cert: File.read('ssl/client-cert.pem'),
-    ssl_client_cert_key: File.read('ssl/client-key.pem')
-  )
+class HelloHandler
+  include KafkaWorker::Handler
+  consumes "hello"
+  @@message_value = nil
+  def handle(message)
+    puts "HelloHandler received the following message: #{message.value}"
+    @@message_value = message.value
+  end
+  
+  def self.message_value
+    @@message_value
+  end
 end
 
-# Set up an asynchronous producer that delivers its buffered messages
-# every ten seconds:
-$kafka_producer = $kafka.async_producer(
-  delivery_interval: 10,
-)
+class CustomError < StandardError
+end
 
-# Make sure to shut down the producer when exiting.
-at_exit { $kafka_producer.shutdown }
-=end
+class ForceErrorHandler
+  include KafkaWorker::Handler
+  consumes "error"
+  
+  @@error_value = nil
+  def handle(message)
+    puts "ForceErrorHandler is going to raise CustomError"
+    raise CustomError, 'default error message'
+  end
+   
+  def on_error(message, err)
+    puts err
+    @@error_value = err
+  end
+  
+  def self.error_value
+    @@error_value
+  end  
+end
 
 describe KafkaWorker do
-  before (:all) do
+  before (:all) do    
     # make a producer to push events to the KafkaWorker
     @kafka = Kafka.new(
       seed_brokers: '127.0.0.1:9092',
@@ -43,63 +52,38 @@ describe KafkaWorker do
       delivery_interval: 1,
     )
     
-=begin    
-    class GoodbyeHandler < KafkaWorker::Handler
-      consumes "goodbye"
-      def handle(message)
-        puts 'got a goodbye message'
-        $goodbyes << 'goodbye'
-      end
-    end
-=end    
-
-  end
-  
-  before(:each) do 
-    # each handler needs a unique class name
-
-  end
-  
-  after(:all) do
-#    Process.kill("QUIT", @kw_pid)
-#    Process.wait
-    @kafka_producer.shutdown
-  end
-
-  it "consume events and pushes a value into a class object" do
-    class HelloHandler < KafkaWorker::Handler
-      consumes "hello"
-      @@received_msg = false
-      def handle(message)
-        puts 'got a hello message'
-        @@received_msg = true
-        puts @@received_msg
-      end
-      
-      def self.received_msg
-        @@received_msg
-      end
-    end  
-
     opts = {
       kafka_ips: ['127.0.0.1:9092'],
       client_id: 'test',
-      group_id:  'test'
+      group_id:  'test',
+      offset_commit_interval: 1
     }
     
-    @kw_pid = fork do
-      kw = KafkaWorker::Worker.new(opts)
-      kw.run
+    @kw = KafkaWorker::Worker.new(opts)    
+    
+    Thread.new do
+      @kw.run
     end
-
-    @kafka_producer.produce('hello', topic:'hello')
-    #sleep 6
-    sleep 4
-    puts HelloHandler.received_msg
-    expect(HelloHandler.received_msg).to eq true
-
-    Process.kill("QUIT", @kw_pid)
-    Process.wait
-
+    # circle ci is slow, need to wait for connection before starting
+    sleep 30
   end
+  
+  after(:all) do
+    @kw.stop_consumer
+    @kafka_producer.shutdown
+  end
+  
+  it "consume events and pushes a value into a class object" do
+    message_value = 'Hello World!'
+    @kafka_producer.produce(message_value, topic:'hello')
+    sleep 30
+    expect(HelloHandler.message_value).to eq message_value
+  end
+
+  it "catch an exception if there is in the handler code" do
+    message_value = 'An error is coming.'
+    @kafka_producer.produce(message_value, topic:'error')
+    sleep 30
+    expect(ForceErrorHandler.error_value.to_s).to eq 'default error message'
+  end  
 end
